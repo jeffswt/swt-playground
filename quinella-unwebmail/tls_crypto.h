3815823daf445191fb8a35e288cc7538e2b343c3
Geoffrey Tang <jeffswt@users.noreply.github.com>
2021-06-28 22:21:00 +0800

    feat(quinella): add tls hmac functions

---
+++ quinella-unwebmail/tls_crypto.h
@@ -0,0 +1,30 @@
+
+#include <iostream>
+#include <openssl/hmac.h>
+#include "tls_cipher_suite.h"
+
+
+/// Calculate HMAC values.
+/// @param proto: HMAC protocol to be used, e.g. SHA256.
+/// @param secret: Secret string, in binary
+/// @param seed: Seed string, in binary
+/// @returns HMAC_proto(secret, seed)
+std::string tls_key_hmachash(CipherHmacProtocol proto, std::string secret,
+    std::string seed);
+
+/// Calculate Phash values.
+/// @param proto: HMAC protocol to be used, e.g. SHA256.
+/// @param secret: Secret string, in binary
+/// @param seed: Seed string, in binary
+/// @returns P_proto(secret, seed)
+std::string tls_key_phash(CipherHmacProtocol proto, std::string secret,
+    std::string seed, int length);
+
+/// Calculate pseudo-random values.
+/// @param proto: HMAC protocol to be used, e.g. SHA256.
+/// @param secret: Secret string, in binary
+/// @param label: Label string, in binary
+/// @param seed: Seed string, in binary
+/// @returns PRF_proto(secret, label, seed)
+std::string tls_key_prf(CipherHmacProtocol proto, std::string secret,
+    std::string label, std::string seed, int length);
