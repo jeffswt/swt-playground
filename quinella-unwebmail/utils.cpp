42a6676796d8a9e237f4a72886bd60d2c811a5ea
Geoffrey Tang <jeffswt@users.noreply.github.com>
2021-06-28 11:00:56 +0800

    feat(quinella): add common utilities

---
+++ quinella-unwebmail/utils.cpp
@@ -0,0 +1,56 @@
+
+#include "utils.h"
+
+
+CapException::CapException(std::string what) {
+    this->_what = what;
+}
+
+const char* CapException::what() const throw() {
+    return this->_what.c_str();
+}
+
+std::string b16encode(std::string in) {
+    static char charmap[17] = "0123456789abcdef";
+    std::string out;
+    for (char chr : in) {
+        out += charmap[(chr >> 4) & 0xf];
+        out += charmap[chr & 0xf];
+    }
+    return out;
+}
+
+// hex_to_int() -- Convert hexadecimal character to integer counterpart.
+// @param ch: Input character, range = 0-9, a-f, A-F.
+// @return Integer counterpart, 0 if the input is invalid.
+int hex_to_int(char ch) {
+    if (ch >= '0' && ch <= '9')
+        return ch - '0';
+    if (ch >= 'a' && ch <= 'f')
+        return ch - 'a' + 10;
+    if (ch >= 'A' && ch <= 'F')
+        return ch - 'A' + 10;
+    return 0;
+}
+
+std::string b16decode(std::string in) {
+    std::string out;
+    for (int i = 0; i < in.length(); i += 2) {
+        char a = in[i], b = in[i + 1];
+        int va = hex_to_int(a), vb = hex_to_int(b);
+        out += (char)(va * 16 + vb);
+    }
+    return out;
+}
+
+int string_to_int(std::string in) {
+    if (in.length() == 0)
+        return -1;
+    int res = 0;
+    for (char ch : in)
+        if (ch >= '0' && ch <= '9')
+            res = res * 10 + ch - '0';
+        else
+            return -1;
+    return res;
+}
---
+++ quinella-unwebmail/utils.h
@@ -0,0 +1,62 @@
+
+#include <iostream>
+#include <vector>
+#include <map>
+
+
+/// Quinella runtime exception.
+class CapException : public std::exception {
+private:
+    std::string _what;
+public:
+    CapException(std::string what);
+    const char* what() const throw();
+};
+
+/// Encodes binary string into hexadecimal format.
+/// @param in: arbitrary binary string
+/// @return hexadecimal string consisting only of 0-9 and a-f,
+///         length guaranteed to be even
+std::string b16encode(std::string in);
+
+/// Decodes b16encode()'d hexadecimal string into binary.
+/// @param in: Hexadecimal string consisting only of 0-9, a-f and A-F.
+///            It's the users' duty to ensure characters are within range
+/// @return Binary string corresponding to input.
+///         Last character chomped if input length is odd.
+///         Illegal character intepreted as 0x0.
+std::string b16decode(std::string in);
+
+/// Converts non-negative integer string to int.
+/// @param in: A string consisting only of 0-9
+/// @return Integer whose value equals to input string.
+///         Returns -1 when illegal characters occur.
+/// @exception Overflow: It's the user's duty to ensure no overflow.
+int string_to_int(std::string in);
+
+/// Print vector of pairs in a Python format.
+/// @param out: Targeted output stream, such as std::cout.
+/// @param in: Vector of pairs to print.
+template <typename _Ta, typename _Tb>
+void visualize_dict(std::ostream &out, std::vector<std::pair<_Ta, _Tb>> in) {
+    out << "{";
+    bool had_items = false;
+    for (auto &pr : in) {
+        if (had_items)
+            out << ", ";
+        out << "'" << pr.first << "': '" << pr.second << "'";
+        had_items = true;
+    }
+    out << "}";
+}
+
+/// Print std::map in a Python format.
+/// @param out: Targeted output stream, such as std::cout.
+/// @param in: Map to print.
+template <typename _Ta, typename _Tb>
+void visualize_dict(std::ostream &out, std::map<_Ta, _Tb> in) {
+    std::vector<std::pair<_Ta, _Tb>> vec;
+    for (auto &pr : in)
+        vec.push_back(pr);
+    visualize_dict(out, vec);
+}
